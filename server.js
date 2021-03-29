8568e6ea87833e01e88d8806490b7526abba09bd
yasmin <yasminfathy630@gmail.com>
2021-03-29 16:44:04 +0200

    deployment

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(express.static(__dirname + "/dist/mega-front"));
 
 const port = process.env.PORT || 4200;
 
-app.listen(port);
+app.listen(port, () => {
+  console.log("server is listening on " + port);
+});
 
 app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/dist/mega-front/index.html");
